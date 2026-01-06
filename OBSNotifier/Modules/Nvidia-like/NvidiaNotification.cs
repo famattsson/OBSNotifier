8bf1d70ceeb1ac2ca867af85fd3821c518ebfc7e
Filip Mattsson <filip.mattson@swegon.com>
2026-01-06 05:07:56 +0100

    Add project files.

---
+++ OBSNotifier/Modules/Nvidia-like/NvidiaNotification.cs
@@ -0,0 +1,135 @@
+﻿using OBSNotifier.Modules.Nvidia_like;
+using System;
+using System.Windows;
+using System.Windows.Automation.Peers;
+
+namespace OBSNotifier.Modules.NvidiaLike
+{
+    public partial class NvidiaNotification : IOBSNotifierModule
+    {
+        internal enum Positions
+        {
+            TopLeft, TopRight,
+        }
+
+        Action<string> logWriter = null;
+        NvidiaNotificationWindow window = null;
+        RecordingIndicator indicator = null;
+
+        public string ModuleID => "Nvidia-Like";
+        public string ModuleName => Utils.Tr("nvidia_like_module_name");
+
+        public string ModuleAuthor => "Dmitriy Salnikov";
+
+        public string ModuleDescription => Utils.Tr("nvidia_like_module_desc");
+
+        public AvailableModuleSettings DefaultAvailableSettings => AvailableModuleSettings.AllNoCustomSettings;
+
+        OBSNotifierModuleSettings _moduleSettings = new OBSNotifierModuleSettings()
+        {
+            UseSafeDisplayArea = true,
+            AdditionalData = "BackgroundColor = #2E48BD\nForegroundColor = #000000\nTextColor = #E4E4E4\nSlideDuration = 400\nSlideOffset = 180\nLineWidth = 6.0\nScale = 1.0\nMaxPathChars = 32\nClickThrough = False\nShowQuickActions = True\nShowQuickActionsColoredLine = True\nQuickActionsOffset = 8.0\nIconHeight = 64.0\nIconPath = INVALID_PATH",
+            Option = Positions.TopRight,
+            Offset = new Point(0, 0.1),
+            OnScreenTime = 3000,
+        };
+
+        public OBSNotifierModuleSettings ModuleSettings
+        {
+            get => _moduleSettings;
+            set => _moduleSettings = value;
+        }
+
+        public Type EnumOptionsType => typeof(Positions);
+
+        public NotificationType DefaultActiveNotifications => NotificationType.All;
+
+        public bool ModuleInit(Action<string> logWriter)
+        {
+            this.logWriter = logWriter;
+            return true;
+        }
+
+        public void ModuleDispose()
+        {
+            window?.Close();
+            window = null;
+        }
+
+        public bool ShowNotification(NotificationType type, string title, string description = null, object[] originalData = null)
+        {
+            if (window == null)
+            {
+                window = new NvidiaNotificationWindow(this);
+                window.Closing += Window_Closing;
+            }
+
+            if (type == NotificationType.RecordingStarted || type == NotificationType.RecordingResumed)
+            {
+                indicator = new RecordingIndicator(this);
+                indicator.ShowIndicator();
+            }
+            else if ((type == NotificationType.RecordingStopped || type == NotificationType.RecordingPaused) && indicator != null)
+            {
+                indicator.Close();
+            }
+            window.ShowNotif(type, title, description);
+
+            return true;
+        }
+
+        public void ShowPreview()
+        {
+            if (window == null)
+            {
+                window = new NvidiaNotificationWindow(this);
+                window.Closing += Window_Closing;
+            }
+
+            window.ShowPreview();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            (sender as NvidiaNotificationWindow).Closing -= Window_Closing;
+
+            if (window == sender)
+                window = null;
+        }
+
+        public void HidePreview()
+        {
+            window?.HidePreview();
+        }
+
+        public void ForceCloseAllRelativeToModule()
+        {
+            if (window != null)
+            {
+                window.Closing -= Window_Closing;
+                window.Close();
+            }
+            window = null;
+        }
+
+        public void OpenCustomSettings() { }
+
+        public string GetCustomSettingsDataToSave() => null;
+
+        public string GetFixedAdditionalData()
+        {
+            return Utils.ConfigFixString<NvidiaCustomAnimationConfig>(_moduleSettings.AdditionalData);
+        }
+
+        public void Log(string txt)
+        {
+            logWriter.Invoke(txt);
+        }
+
+        public void Log(Exception ex)
+        {
+            Log($"Exception:\n{ex.Message}\nStackTrace:\n{ex.StackTrace}");
+        }
+    }
+}
+
---
+++ OBSNotifier/Modules/Nvidia-like/RecordingIndicator.xaml.cs
@@ -0,0 +1,98 @@
+﻿using OBSNotifier.Modules.NvidiaLike;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OBSNotifier.Modules.Nvidia_like
+{
+    /// <summary>
+    /// Interaction logic for RecordingIndicator.xaml
+    /// </summary>
+    public partial class RecordingIndicator : Window
+    {
+        const double default_window_width = 90;
+        const double default_window_height = 90;
+
+        NvidiaCustomAnimationConfig currentParams;
+        NvidiaCustomAnimationConfig previousParams;
+        readonly NvidiaCustomAnimationConfig defaultParams = new NvidiaCustomAnimationConfig();
+
+        NvidiaNotification owner;
+        private int addDataHash = -1;
+
+        bool IsPositionedOnTop { get => (NvidiaNotification.Positions)owner.ModuleSettings.Option == NvidiaNotification.Positions.TopRight; }
+
+
+        public RecordingIndicator(NvidiaNotification module)
+        {
+            currentParams = new NvidiaCustomAnimationConfig();
+            owner = module;
+            InitializeComponent();
+        }
+
+        void UpdateParameters()
+        {
+
+            if (owner.ModuleSettings.AdditionalData != null && owner.ModuleSettings.AdditionalData.GetHashCode() != addDataHash)
+            {
+                addDataHash = owner.ModuleSettings.AdditionalData.GetHashCode();
+
+                // Recreate but remember preview state
+                bool prev = currentParams.IsPreviewNotif;
+                currentParams = new NvidiaCustomAnimationConfig()
+                {
+                    IsPreviewNotif = prev,
+                };
+                Utils.ConfigParseString(owner.ModuleSettings.AdditionalData, ref currentParams);
+            }
+            // General params
+            currentParams.Duration = owner.ModuleSettings.OnScreenTime;
+            currentParams.IsOnRightSide = (NvidiaNotification.Positions)owner.ModuleSettings.Option == NvidiaNotification.Positions.TopRight;
+
+            UtilsWinApi.SetWindowIgnoreMouse(this.GetHandle(), currentParams.ClickThrough && !currentParams.ShowQuickActions);
+
+
+            // Sizes
+
+            Width = Math.Ceiling(currentParams.LineWidth * currentParams.Scale);
+            Height = Math.Ceiling(default_window_height * currentParams.Scale);
+
+            // Position
+            var pe = (NvidiaNotification.Positions)owner.ModuleSettings.Option;
+            var anchor = (Utils.AnchorPoint)Enum.Parse(typeof(Utils.AnchorPoint), pe.ToString());
+            Point pos = Utils.GetWindowPosition(anchor, new Size(Width, Height), owner.ModuleSettings.Offset);
+
+            Left = pos.X;
+            Top = pos.Y;
+        }
+
+
+        public void ShowIndicator()
+        {
+            if (currentParams.IsPreviewNotif)
+                return;
+
+            previousParams = currentParams.Duplicate();
+            UpdateParameters();
+
+            if (!IsPositionedOnTop)
+            {
+                var delta = Height - ActualHeight;
+                if (delta > 0)
+                    Top += delta;
+            }
+
+            Show();
+        }
+    }
+}
